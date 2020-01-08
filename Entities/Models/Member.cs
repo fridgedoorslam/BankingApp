e68ab1666aff586037a3543ed14f29f96963be75
Trevor Carlson <trevor.carlson.mn@gmail.com>
2020-01-07 18:32:19 -0600

    Add project files.

---
+++ Entities/Models/Member.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Entities.Models
+{
+    public class Member : DbContext
+    {
+        public int MemberId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
